3b7a663be0d372a6cb18ca3f33ffbebb7b2bdd63
halil <halilbadem47@gmail.com>
2023-04-09 02:29:01 +0300

    Add user slice to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../features/counter/userSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    user: userReducer,
+  },
+  devTools: {
+    name: 'MyApp',
+    trace: true,
+    traceLimit: 25,
   },
 });
